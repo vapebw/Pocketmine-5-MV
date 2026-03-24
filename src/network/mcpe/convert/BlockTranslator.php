e77e72af6820aa9c5847882c6065cd7fd5d1b635
Vapesito <jbinact@gmail.com>
2026-03-24 12:34:40 -0700

    feat(protocol): add 1.26.10 block state mappings (reuse 1.26.0)

--- src/network/mcpe/convert/BlockTranslator.php
+++ src/network/mcpe/convert/BlockTranslator.php
@@ -46,6 +46,10 @@ final class BlockTranslator{
 			self::CANONICAL_BLOCK_STATES_PATH => '',
 			self::BLOCK_STATE_META_MAP_PATH => '',
 		],
+		ProtocolInfo::PROTOCOL_1_26_0 => [
+			self::CANONICAL_BLOCK_STATES_PATH => '',
+			self::BLOCK_STATE_META_MAP_PATH => '',
+		],
 		ProtocolInfo::PROTOCOL_1_21_130 => [
 			self::CANONICAL_BLOCK_STATES_PATH => '',
 			self::BLOCK_STATE_META_MAP_PATH => '',
