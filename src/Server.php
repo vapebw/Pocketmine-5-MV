10c0e0cd8b149a2d500b8f6bafc3ccc4e9737221
vape <jbinact@gmail.com>
2026-03-30 20:54:51 -0700

    Update Server.php

--- src/Server.php
+++ src/Server.php
@@ -2,18 +2,18 @@
 
 /*
  *
- *  ____                                _   _  ____ _____   __  __ ____
- * |  _ \  ___ _ __ ___   ___  _ __   | | | |/ ___|  ___| |  \/  |  _ \
- * | | | |/ _ \ '_ ` _ \ / _ \| '_ \  | |_| | |   | |_    | |\/| | |_) |
- * | |_| |  __/ | | | | | (_) | | | | |  _  | |___|  _|   | |  | |  __/
- * |____/ \___|_| |_| |_|\___/|_| |_| |_| |_|\____|_|     |_|  |_|_|
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author Vfdam
+ * @author PocketMine Team
  * @link http://www.pocketmine.net/
  *
  *
